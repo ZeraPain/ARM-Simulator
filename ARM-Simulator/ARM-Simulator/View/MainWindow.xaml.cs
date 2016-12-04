6fb66cd619c6b1b9c779cc2e083cf6afa94419c2
Anja <Anja@DESKTOP-NSPTPBO>
2016-12-04 17:45:23 +0100

    Editor

--- ARM-Simulator/ARM-Simulator/View/MainWindow.xaml.cs
+++ ARM-Simulator/ARM-Simulator/View/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,109 @@ namespace ARM_Simulator
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+       
+
+
+
+        private void MenuNew_Click(object sender, RoutedEventArgs e)
+        {
+            var start = TxtEditor.Document.ContentStart;
+            var end = TxtEditor.Document.ContentEnd;
+            var isEmpty = start.GetOffsetToPosition(end);
+
+          if (isEmpty > 2)
+            {
+                var result = MessageBox.Show("Would you like to save your File?", "Arm Simulator",
+                    MessageBoxButton.YesNo);
+                switch (result)
+                {
+                    case MessageBoxResult.Yes:
+                        MenuSave_OnClick();
+                        break;
+                    case MessageBoxResult.No:
+                        TxtEditor.Document.Blocks.Clear();
+                        break;
+                }
+            }
+        }
+
+        private void MenuSave_OnClick()
+        {
+            var saveFile = new SaveFileDialog
+            {
+                Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*"
+            };
+
+            if (saveFile.ShowDialog() != true) return;
+            try
+            {
+                var fileStream = new FileStream(saveFile.FileName, FileMode.Create);
+                var range = new TextRange(TxtEditor.Document.ContentStart, TxtEditor.Document.ContentEnd);
+                range.Save(fileStream, DataFormats.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error: Saving your File!");
+            }
+        }
+
+        private void MenuOpen_Click(object sender, RoutedEventArgs e)
+        {
+            var openFile = new OpenFileDialog
+            {
+                Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*"
+            };
+
+            if (openFile.ShowDialog() != true) return;
+            try
+            {
+                var fileStream = new FileStream(openFile.FileName, FileMode.Open);
+                var range = new TextRange(TxtEditor.Document.ContentStart,
+                    TxtEditor.Document.ContentEnd);
+                range.Load(fileStream, DataFormats.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error: Loading your File");
+            }
+        }
+
+        private void SaveCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void SaveCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+         //Save File via Shortcut 
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Actually  == Save As
+        private void MenuSave_OnClick(object sender, RoutedEventArgs e)
+        {
+            var saveFile = new SaveFileDialog
+            {
+                Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*"
+            };
+
+            if (saveFile.ShowDialog() != true) return;
+            try
+            {
+                var fileStream = new FileStream(saveFile.FileName, FileMode.Create);
+                var range = new TextRange(TxtEditor.Document.ContentStart, TxtEditor.Document.ContentEnd);
+                range.Save(fileStream, DataFormats.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error: Saving your File!");
+            }
         }
     }
 }
