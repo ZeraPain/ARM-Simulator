4a66694ed5cd104fda5bbec812693d58bb39aba6
Anja <Anja@DESKTOP-NSPTPBO>
2016-12-25 17:35:52 +0100

    Statusbar Improvements

---
+++ ARM-Simulator/ARM-Simulator/ViewModel/Converters/StatusConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using ARM_Simulator.Annotations;
+
+namespace ARM_Simulator.ViewModel.Converters
+{
+   internal class StatusConverter : IValueConverter
+    {
+        [CanBeNull]
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var debugmode = value as bool?;
+            var image = parameter as string;
+         
+            if (debugmode == null || parameter == null) return null;
+
+            switch (image)
+            {
+                case "Editor":
+                    return debugmode == true ? "/Resources/Images/on.png" : "/Resources/Images/off.png";      
+                case "Debugger":
+                    return debugmode== true ? "/Resources/Images/off.png" : "/Resources/Images/on.png";
+            }
+
+            return null;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ ARM-Simulator/ARM-Simulator/ViewModel/Converters/StatusTextConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using ARM_Simulator.Annotations;
+
+namespace ARM_Simulator.ViewModel.Converters
+{
+   internal class StatusTextConverter : IValueConverter
+    {
+        [CanBeNull]
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var debugmode = value as bool?;
+            var text = parameter as string;
+
+            if (debugmode == null || parameter == null) return null;
+
+            switch (text)
+            {
+                case "Editor":
+                    return debugmode == true ? "Debugmode" : "Editor";
+                case "Debugger":
+                    return debugmode == true ? "Editor" : "Debugmode";
+            }
+
+            return null;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
+
