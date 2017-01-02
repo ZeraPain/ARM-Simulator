e6b068a5df399cf2f498edbe5d0fc4c662ebc9a1
Anja <Anja@DESKTOP-NSPTPBO>
2017-01-02 21:45:40 +0100

    changes Button size, Icon for Application, Icon for exe

---
+++ ARM-Simulator/ARM-Simulator/Model/Components/Stack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using ARM_Simulator.Annotations;
+
+namespace ARM_Simulator.Model.Components
+{
+    public class Stack : INotifyPropertyChanged
+    {
+      
+       
+
+        public Stack()
+        {
+           
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected void OnPropertyChanged([CanBeNull] [CallerMemberName] string propertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    
+}
+}
