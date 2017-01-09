c318a4655b9960e6c447fd48fb5ac461805bcd2e
dave <david.tarnow@outlook.de>
2017-01-09 17:49:34 +0100

    fixed conversion bug

--- ARM-Simulator/ARM-Simulator/ViewModel/Converters/DisplayValueConverter.cs
+++ ARM-Simulator/ARM-Simulator/ViewModel/Converters/DisplayValueConverter.cs
@@ -30,9 +30,6 @@ namespace ARM_Simulator.ViewModel.Converters
             return valueString;
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => value;
     }
 }
